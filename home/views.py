3d60d9340ba555d06012978ce76fa64ff8f26ad0
Büşra <b21426807.cs.hacettepe.edu.tr>
2018-12-17 00:28:41 +0300

    Update views.py

--- home/views.py
+++ home/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 
 def home_view(request):
-    return render(request, 'home.html', {})
+    if request.user.is_authenticated:
+        context = {
+            'user': 'Büşra',
+        }
+    else:
+        context = {
+            'user': 'Guest',
+        }
+    return render(request, 'home.html', context)
