0a304d6fdb9cf28d76473387e068f52bb6d4b37a
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2019-01-18 22:08:46 +0300

    Update models.py

--- post/models.py
+++ post/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-
-# Create your models here.
+from django.urls import reverse
 
 
 class Post(models.Model):
     title = models.CharField(max_length=120,verbose_name="Title")
     text = models.TextField(verbose_name="Content")
-    DateOfPublished = models.DateTimeField(verbose_name="Published Date")
+    DateOfPublished = models.DateTimeField(verbose_name="Published Date", auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post:detail', kwargs={'id': self.id})
